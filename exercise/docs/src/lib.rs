5551b05d525f0242abcb5fcdbddbf26d2a820654
Cullen McClellan <cullen.mcclellan@thoughtworks.com>
2024-04-30 15:46:02 -0400

    Complete docs exercise

--- exercise/docs/src/lib.rs
+++ exercise/docs/src/lib.rs
@@ -15,21 +15,38 @@
 // - Document the "roundness" field, explaining that it is a percentage
 // - Document the "orangeness" field, explaining that it is a number from 8 to 27
 
+//! A pumpkin, in English-language vernacular, is a cultivated winter squash in the genus Cucurbita.
+//! The term is most commonly applied to round, orange-colored squash varieties, though it does not 
+//! possess a scientific definition and may be used in reference to many different squashes of varied 
+//! appearance.
+//! 
+//! ![french market pumpkin](https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/FrenchMarketPumpkinsB.jpg/700px-FrenchMarketPumpkinsB.jpg)
+
+/// Big orange thing
+/// 
+/// # Recipes
+/// 
+/// Coming soon...
 pub struct Pumpkin {
+    /// percentage
     pub roundness: f32,
-    pub orangeness: i32,
+    /// number from 8 to 27
+    pub orangeness: i32, 
 }
 
 // 4. Document the "smash" method. Explain that if you smash the pumpkin, it will be gone. Then it
 // can't be used for pie. :'-(
 
 impl Pumpkin {
+    /// If you smash the pumpkin, it will be gone. Then it
+    /// can't be used for pie. :'-(
     pub fn smash(self) {}
 }
 
 // 5. Document that BURNT_ORANGE is for the "orangeness" field in the Pumpkin struct.
 // - Link to the Pumpkin struct in your description
 
+/// Color for the orangeness field, see [`Pumpkin`]
 pub const BURNT_ORANGE: i32 = 13;
 
 // Challenge: Find the option to pass to `cargo doc` so that documentation for this private item
